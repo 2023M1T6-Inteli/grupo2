2080caeb5005996d5ae73e1cdbe2388fcf809658
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-02-15 13:47:12 -0300

    refactor: melhora a movimentação do personagem
    Visando um código limpo, foi necessário reescrever a movimentação, de modo a torná-la mais clara

--- src/FirstClass/Scripts/Player.gd
+++ src/FirstClass/Scripts/Player.gd
@@ -1,38 +1,16 @@
 extends KinematicBody2D
 
-export var walkSpeed = 4
-const TILE_SIZE = 16
-
-onready var animationTree = $AnimationTree  # animações criadas
-onready var animationState = animationTree.get("parameters/playback")  # pode ser idle ou walk
-
-
-var initialPosition = Vector2(0, 0)  # Posição inicial (ou atual) do sprite
-var inputDirection = Vector2(0, 0)  # Movimento do sprite de acordo com as teclas pressionadas
-var isMoving = false  # Serve como checagem da movimentação do sprite
-var percentMovedToNextTile = 0.0  # Verifica a porcentagem de movimentação do sprite
+export var walkSpeed = 50  # Rapidez do movimento
+var velocity = Vector2.ZERO  # Vetor responsável pela movimentação do personagem
+var inputDirection = Vector2(0, 0)  # Vetor atualizado de acordo com as teclas pressionadas
+onready var screenSize = get_viewport_rect().size  # Tamanho da tela
 
 
-# Chamada no início do jogo
 func _ready():
-	initialPosition = position
+	pass
 
 
-# Função responsável por verificar, constantemente, o status do personagem
-# Além de mudar a animação (walk quando caminha e idle quando para)
 func _physics_process(delta):
-	if isMoving == false:
-		process_player_input()
-	elif inputDirection != Vector2.ZERO:
-		animationState.travel("Walk")  # muda animação para walk (quando estiver andando)
-		move(delta)
-	else:
-		animationState.travel("Idle")  # muda animação para idle (quando estiver parado)
-		isMoving = false 
-
-
-# Função que processa o input do usuário (direita, esquerda, cima e baixo)
-func process_player_input():
 	# Processa e salva inputs na horizontal e na vertical
 	if inputDirection.y == 0:
 		inputDirection.x = (
@@ -42,42 +20,16 @@ func process_player_input():
 		inputDirection.y = (
 				int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 		)
-
-	# Se o vetor de direção não for 0, significa que houve input do usuário, logo, is_moving = true
+	
+	# Se o vetor inputDirection não for 0, siginifica que há movimentação
 	if inputDirection != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", inputDirection)
-		animationTree.set("parameters/Walk/blend_position", inputDirection)
-		initialPosition = position
-		isMoving = true
+		velocity = inputDirection
 	else:
-		animationState.travel("Idle")
-
-
-# Responsável pela movimentação do personagem
-func move(delta):
-	percentMovedToNextTile += walkSpeed * delta
+		velocity = Vector2.ZERO
 	
-	# Incrementa a posição do personagem, de acordo com a posição inicial, com o input do usuário e
-	# com o tamanho do tile (16px)
-	if percentMovedToNextTile >= 1.0:
-		position = initialPosition + (TILE_SIZE * inputDirection)
-		percentMovedToNextTile = 0.0
-		isMoving = false
-	else:
-		position = initialPosition + (TILE_SIZE * inputDirection * percentMovedToNextTile)
+	# Restringe a movimentação do personagem de acordo com o tamanho da tela
+	position.x = clamp(position.x, 0, screenSize.x -16)
+	position.y = clamp(position.y, 0, screenSize.y - 16)
 	
-	# Se o personagem chegar no final da tela (lado direito) horizontal, não permite que ele passe
-	if position.x >= 650:
-		position.x = 650
-	
-	# Se o personagem chegar no final da tela (lado esquerdo) horizontal, não permite que ele passe
-	if position.x <= -530:
-		position.x = -530
-	
-	# Se o personagem chegar no final da tela (cima) vertical, não permite que ele passe
-	if position.y <= -390:
-		position.y = -390
-	
-	# Se o personagem chegar no final da tela (baixo) vertical, não permite que ele passe
-	if position.y >= 345:
-		position.y = 345
+	# Movimenta o personagem de acordo com o vetor velocity vezes a rapidez do movimento
+	velocity = move_and_slide(velocity * walkSpeed)
