f8d6c3d7f7839eeec415831dfcc20f9743c47f95
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-02-20 20:34:30 -0300

    feat: altera volume da música de acordo com o slider

--- src/FirstClass/Scripts/MenuOpcoes.gd
+++ src/FirstClass/Scripts/MenuOpcoes.gd
@@ -4,3 +4,8 @@ extends Node2D
 # Retorna para a tela do menu inicial
 func _on_VoltarButton_pressed():
 	get_tree().change_scene("res://Scenes/MenuInicial.tscn")
+
+
+
+func _on_VolumeSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
