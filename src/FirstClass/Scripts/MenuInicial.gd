522033418c581596784b62eecae348d0c45402fc
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-02-20 20:56:30 -0300

    feat: direciona para a tela do menu inicial

--- src/FirstClass/Scripts/MenuInicial.gd
+++ src/FirstClass/Scripts/MenuInicial.gd
@@ -8,3 +8,9 @@ func _ready():
 # Direciona para a visão exterior da casa
 func _on_JogarButton_pressed():
 	get_tree().change_scene("res://Scenes/CasaExterior.tscn")
+
+
+# Direciona para o menu de opções
+func _on_ConfigButton_pressed():
+	get_tree().change_scene("res://Scenes/MenuOpcoes.tscn")
+	
