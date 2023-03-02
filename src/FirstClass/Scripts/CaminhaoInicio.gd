6366729d1fe7c213b668353858edc06d52b4cc57
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-01 21:18:36 -0300

    feat: implementa aleatoriedade no spawn dos caminhões

--- src/FirstClass/Scripts/CaminhaoInicio.gd
+++ src/FirstClass/Scripts/CaminhaoInicio.gd
@@ -1,10 +1,14 @@
-extends KinematicBody2D
+extends RigidBody2D
+
+var minSpeed = 150
+var maxSpeed = 300
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	
-func _process(delta):
-	move_and_slide(Vector2(0, 100))
+	var tipoCaminhoes = $Sprite.frames.get_animation_names()
+	$Sprite.animation = tipoCaminhoes[randi() % tipoCaminhoes.size()]	
 
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- src/FirstClass/Scripts/MenuInicial.gd
+++ src/FirstClass/Scripts/MenuInicial.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+const caminhao = preload("res://Scenes/CaminhaoInicio.tscn")
+
 
 func _ready():
-	pass
+	randomize()
 
 
 # Direciona para a visão exterior da casa
@@ -13,4 +15,17 @@ func _on_JogarButton_pressed():
 # Direciona para o menu de opções
 func _on_ConfigButton_pressed():
 	get_tree().change_scene("res://Scenes/MenuOpcoes.tscn")
+
+
+func _on_TimerCaminhao_timeout():
+	$PathEsquerda/PathFollow2D.offset = randi()
+	$PathDireita/PathFollow2D.offset = randi()
+	var novoCaminhaoEsquerda = caminhao.instance()
+	var novoCaminhaoDireita = caminhao.instance()
+	add_child(novoCaminhaoEsquerda)
+	add_child(novoCaminhaoDireita)
+	novoCaminhaoEsquerda.position = $PathEsquerda/PathFollow2D.position
+	novoCaminhaoDireita.position = $PathDireita/PathFollow2D.position
+	novoCaminhaoEsquerda.linear_velocity = Vector2(0, rand_range(novoCaminhaoEsquerda.minSpeed, novoCaminhaoEsquerda.maxSpeed))
+	novoCaminhaoDireita.linear_velocity = Vector2(0, rand_range(novoCaminhaoDireita.minSpeed, novoCaminhaoDireita.maxSpeed))
 	
