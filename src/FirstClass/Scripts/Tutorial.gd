b7c59ca2f25e28f81885ff66bd52126975855701
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-02 21:52:03 -0300

    feat: cria versão inicial da tela do tutorial
    Adição do mapa, das setas e da câmera

---
+++ src/FirstClass/Scripts/Tutorial.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	key_pressed()
+
+
+# Altera animação das setas de acordo com a tecla pressionada
+func key_pressed():
+	if Input.is_action_pressed("ui_up"):
+		$Cima.play("pressionada")
+	else:
+		$Cima.play("default")
+	if Input.is_action_pressed("ui_down"):
+		$Baixo.play("pressionada")
+	else:
+		$Baixo.play("default")
+	if Input.is_action_pressed("ui_left"):
+		$Esquerda.play("pressionada")
+	else:
+		$Esquerda.play("default")
+	if Input.is_action_pressed("ui_right"):
+		$Direita.play("pressionada")
+	else:
+		$Direita.play("default")
