4fb217bcc16a4adc17ea83df628dd29acf713005
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-05 19:23:49 -0300

    feat: inicia a construção da HUD com a barra de energia

--- src/FirstClass/Scripts/Global.gd
+++ src/FirstClass/Scripts/Global.gd
@@ -1,5 +1,6 @@
 extends Node
 
 var gameBaseSpeed = 5
-var counter = 0
-var choices:= [] # Array que recebe os dados de cada escolha. 1 = ruim, 0 = boa
+var points = 0
+var choices := [] # Array que recebe os dados de cada escolha. 1 = ruim, 0 = boa
+var energy := 4 # Valor da barra de energia, usada na HUD
