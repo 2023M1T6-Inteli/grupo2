b6ff64a67c1f44cdf1a66e482edcf0764e6ec668
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-06 14:23:25 -0300

    feat: adiciona novos elementos ao menu de opções

--- src/CelsoDrive/Scripts/RoadTinhoso.gd
+++ src/CelsoDrive/Scripts/RoadTinhoso.gd
@@ -5,10 +5,21 @@ var inputDirection = Vector2(0, 0)  # Vetor atualizado de acordo com as teclas p
 var backgroundSpeed = Global.gameBaseSpeed * 0.9 # Velocidade com que o background se move (90% da velocidade dos carros)
 const enemy = preload("res://Scenes/EnemyCar.tscn")
 var spawnPositions
+var finishedDialog = false
 
 
 func _ready():
+	var dialog = Dialogic.start("minigame-tinhoso-tutorial")
+	dialog.connect("dialogic_signal", self, "dialog_listener")
+	add_child(dialog)
 	spawnPositions = $spawn_carros/spawn_positions.get_children()
+
+
+func dialog_listener(string):
+	match string:
+		# Adiciona na lista de escolhas a decisão ruim de aceitar a corrida do tinhoso
+		"finishedDialog":
+			finishedDialog = true
 
 
 func _process(delta):
@@ -17,7 +28,7 @@ func _process(delta):
 		$Road.position.y = -8
 	else:
 		$Road.position.y += backgroundSpeed
-		
+
 
 func cars_timer():
 	yield(get_tree().create_timer(1.0), "timeout")
@@ -25,7 +36,8 @@ func cars_timer():
 
 
 func _on_Timer_timeout():
-	car_spawn()
+	if finishedDialog:
+		car_spawn()
 
 
 func car_spawn():
