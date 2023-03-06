08e18d7a93120ca80c66987a24fc224b49e88f76
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-06 18:23:42 -0300

    feat: mostra infos dinamicamente na tela de opções

--- src/CelsoDrive/Scripts/SettingsMenu.gd
+++ src/CelsoDrive/Scripts/SettingsMenu.gd
@@ -9,3 +9,31 @@ func _on_BackButton_pressed():
 # Altera volume da música de acordo com a mudança no valor do VolumeSlider
 func _on_MusicVolumeSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_AudioLabel_pressed():
+	$SettingsAudioContainer.visible = true
+	$SettingsLanguageContainer.visible = false
+	$SettingsControlsContainer.visible = false
+	$SettingsVideoContainer.visible = false
+
+
+func _on_LanguageLabel_pressed():
+	$SettingsAudioContainer.visible = false
+	$SettingsLanguageContainer.visible = true
+	$SettingsControlsContainer.visible = false
+	$SettingsVideoContainer.visible = false
+#
+#
+func _on_ControlsLabel_pressed():
+	$SettingsAudioContainer.visible = false
+	$SettingsLanguageContainer.visible = false
+	$SettingsControlsContainer.visible = true
+	$SettingsVideoContainer.visible = false
+
+
+func _on_VideoLabel_pressed():
+	$SettingsAudioContainer.visible = false
+	$SettingsLanguageContainer.visible = false
+	$SettingsControlsContainer.visible = false
+	$SettingsVideoContainer.visible = true
