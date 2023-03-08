8b7afc0cf4c5b65f48eb818137ac8dc86b7ed0ca
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-08 09:01:32 -0300

    refactor: comenta script do menu inicial

--- src/CelsoDrive/Scripts/MainMenu.gd
+++ src/CelsoDrive/Scripts/MainMenu.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
-const truck = preload("res://Scenes/MainTruck.tscn")
-var language = Global.selectedLanguage
+const truck = preload("res://Scenes/MainTruck.tscn") # Carrega cena dos caminhões
+var language = Global.selectedLanguage # Carrega informações da variável global de idioma
 
 
 func _ready():
-	# Elementos em inglês
+	# Traduz elementos da tela atual para inglês
 	if language == 1:
 		$TitleLabel.text = "Celso Drive"
 		$PlayButton.text = "Play"
 		$ConfigButton.text = "Settings"
 		$CreditsButton.text = "Credits"
-	MusicController.play_music()
+	MusicController.play_music() # Reproduz música
 	randomize()
 
 
@@ -25,6 +25,7 @@ func _on_ConfigButton_pressed():
 	return get_tree().change_scene("res://Scenes/SettingsMenu.tscn")
 
 
+# Randomiza local de aparecimento e velocidade dos caminhões do background da tela
 func _on_TruckTimer_timeout():
 	$PathLeft/PathFollow2D.offset = randi()
 	$PathRight/PathFollow2D.offset = randi()
@@ -36,4 +37,3 @@ func _on_TruckTimer_timeout():
 	newTruckRight.position = $PathRight/PathFollow2D.position
 	newTruckLeft.linear_velocity = Vector2(0, rand_range(newTruckLeft.minSpeed, newTruckLeft.maxSpeed))
 	newTruckRight.linear_velocity = Vector2(0, rand_range(newTruckRight.minSpeed, newTruckRight.maxSpeed))
-	
