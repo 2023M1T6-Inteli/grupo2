1c163e89fe6c1fc7c8c69ceaf2d7a28c2023ec85
Victor Gabriel Marques <victor.marques@sou.inteli.edu.br>
2023-03-08 09:10:47 -0300

    refactor: comenta script da cena do lado de fora da casa

--- src/CelsoDrive/Scripts/OutsideHouse.gd
+++ src/CelsoDrive/Scripts/OutsideHouse.gd
@@ -1,13 +1,6 @@
 extends Node2D
 
 
-func _ready():
-	pass
-
-
+# Move o personagem para dentro de casa
 func _on_CelsoHouse_body_entered(body):
 	return get_tree().change_scene("res://Scenes/InsideHouse.tscn")
-
-
-func _on_CelsoHouse_body_exited(body):
-	$InteractKey.play("default")
