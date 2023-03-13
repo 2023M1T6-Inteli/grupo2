631cef4115902d8a02f535660f1eb82d5c0546ab
VictorGM01 <victormarques8801@gmail.com>
2023-03-13 20:53:40 -0300

    feat: cria cenas para as variantes do Celso

---
+++ src/CelsoDrive/Scripts/CelsoSelect.gd
@@ -0,0 +1,7 @@
+extends Control
+
+# Carrega cenas das variantes do Celso
+onready var celso1 = preload("res://Scenes/Player01.tscn")
+onready var celso2 = preload("res://Scenes/Player02.tscn")
+onready var celso3 = preload("res://Scenes/Player03.tscn")
+onready var celso4 = preload("res://Scenes/Player04.tscn")
--- src/CelsoDrive/Scripts/Global.gd
+++ src/CelsoDrive/Scripts/Global.gd
@@ -7,3 +7,4 @@ var energy := 4 # Valor da barra de energia, usada na HUD
 var pausedGame = false # Variável de parada para o minigame do tinhoso
 var selectedMusic = 0 # O = forró, 1 = sertanejo, 2 = rock e 3 = samba
 var selectedLanguage = 0 # 0 = português, 1 = inglês e 2 = espanhol
+var playerDir # Caminho da cena do personagem selecionado
