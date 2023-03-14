6b1008478f1fe8145656e6c80ce265c393a3b888
Gabriel Farias <gabrielfalves2003@gmail.com>
2023-03-14 14:51:54 -0300

    feat: freezer interaction code comments

--- src/CelsoDrive-Desenvolvimento/Scripts/InsideHouse.gd
+++ src/CelsoDrive-Desenvolvimento/Scripts/InsideHouse.gd
@@ -5,13 +5,16 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	# Verifica se o jogador apertou a tecla E (interação) dentro da área da geladeira, caso positivo, mostra a cena da geladeira
 	if(Input.is_action_pressed("interagir") && $FreezerArea/FreezerE.visible == true):
-		$Freezer.visible = true;
+		$FreezerArea/Freezer.visible = true;
 
 func _on_FreezerArea_body_entered(body):
+	# Ao entrar na área da geladeira exibe o comando para interagir
 	$FreezerArea/FreezerE.visible = true
 
 
 func _on_FreezerArea_body_exited(body):
+	# Ao sair da área da geladeira esconde o comando para interagir e a cena da geladeira (caso esteja aberta)
 	$FreezerArea/FreezerE.visible = false
-	$Freezer.visible = false
+	$FreezerArea/Freezer.visible = false
