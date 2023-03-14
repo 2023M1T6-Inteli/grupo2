55d7f98713de3b18d85e40337b872097bd9f63ad
Gabriel Farias <gabrielfalves2003@gmail.com>
2023-03-14 15:27:12 -0300

    feat: table interaction connected to house

--- src/CelsoDrive-Desenvolvimento/Scripts/InsideHouse.gd
+++ src/CelsoDrive-Desenvolvimento/Scripts/InsideHouse.gd
@@ -8,11 +8,14 @@ func _process(delta):
 	# Verifica se o jogador apertou a tecla E (interação) dentro da área da geladeira, caso positivo, mostra a cena da geladeira
 	if(Input.is_action_pressed("interagir") && $FreezerArea/FreezerE.visible == true):
 		$FreezerArea/Freezer.visible = true;
+		
+	# Verifica se o jogador apertou a tecla E (interação) dentro da área da mesa, caso positivo, mostra a cena da mesa
+	if(Input.is_action_pressed("interagir") && $TableArea/TableE.visible == true):
+		$TableArea/Table.visible = true;
 
 func _on_FreezerArea_body_entered(body):
 	# Ao entrar na área da geladeira exibe o comando para interagir
 	$FreezerArea/FreezerE.visible = true
-
 
 func _on_FreezerArea_body_exited(body):
 	# Ao sair da área da geladeira esconde o comando para interagir e a cena da geladeira (caso esteja aberta)
@@ -22,8 +25,8 @@ func _on_FreezerArea_body_exited(body):
 func _on_TableArea_body_entered(body):
 	# Ao entrar na área da mesa exibe o comando para interagir
 	$TableArea/TableE.visible = true
-	
 
 func _on_TableArea_body_exited(body):
 	# Ao sair da área da mesa esconde o comando para interagir e a cena da mesa (caso esteja aberta)
 	$TableArea/TableE.visible = false
+	$TableArea/Table.visible = false
