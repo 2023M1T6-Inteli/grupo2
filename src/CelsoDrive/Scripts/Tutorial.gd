5187b5553567f291d71a8301301d7162acd2cfef
VictorGM01 <victormarques8801@gmail.com>
2023-03-20 18:50:23 -0300

    feat: adiciona interação com o Tinhoso no tutorial

--- src/CelsoDrive/Scripts/Tutorial.gd
+++ src/CelsoDrive/Scripts/Tutorial.gd
@@ -30,12 +30,27 @@ func _ready():
 func _process(_delta):
 	# Verifica, a cada frame, as teclas pressionadas
 	key_pressed()
+	
+	# Chama a função que movimenta os npcs
 	move_npc()
 	
+	# Inicia interação com a casa
 	if $Houses/Area2D/EInteract.visible == true && Input.is_action_just_pressed("interagir"):
 		dialog = Dialogic.start("tutorial-house")
 		dialog.connect("dialogic_signal", self, "dialog_listener")
 		add_child(dialog)
+		
+		
+	if $TinhosoSceneArea/EInteract.visible == true and Input.is_action_just_pressed("interagir"):
+		# Seleciona o diálogo com o tinhoso de acordo com o idioma do jogo
+		if language == 1:
+			dialog = Dialogic.start("tinhoso-1-en")
+			dialog.connect("dialogic_signal", self, "dialog_listener")
+			add_child(dialog)
+		else:
+			dialog = Dialogic.start("tinhoso-1")
+			dialog.connect("dialogic_signal", self, "dialog_listener")
+			add_child(dialog)
 
 
 # Recebe e trata os sinais do nó de diálogo
@@ -52,22 +67,10 @@ func dialog_listener(string):
 		# Quando o sinal for emitido, a variável finishedDialog recebe true
 		"finishedDialog":
 			finishedDialog = true;
-			
+		
+		# Exibe tela com mensagem no celular após o diálogo de interação com a casa
 		"interacted":
 			$Message.visible = true;
-
-
-# Aciona diálogo com tinhoso quando o personagem entra na Area2D
-func _on_NextSceneArea_body_entered(_body):
-	# Seleciona o diálogo de acordo com o idioma do jogo
-	if language == 1:
-		dialog = Dialogic.start("tinhoso-1-en")
-		dialog.connect("dialogic_signal", self, "dialog_listener")
-		add_child(dialog)
-	else:
-		dialog = Dialogic.start("tinhoso-1")
-		dialog.connect("dialogic_signal", self, "dialog_listener")
-		add_child(dialog)
 
 
 # Altera animação das setas de acordo com a tecla pressionada
@@ -104,13 +107,26 @@ func move_npc():
 		$TutorialNpcCat/AnimatedSprite.play("RunRight")
 
 
+# Mostra a tecla E quando chega próximo da casa
 func _on_Area2D_body_entered(_body):
 	$Houses/Area2D/EInteract.visible = true
 
 
+# Esconde a tecla E quando sai para longe da casa
 func _on_Area2D_body_exited(_body):
 	$Houses/Area2D/EInteract.visible = false
 
 
+# Fecha tela de mensagem
 func _on_CloseMessageButton_pressed():
 	$Message.visible = false;
+
+
+# Mostra a tecla E quando chega próximo do tinhoso
+func _on_TinhosoSceneArea_body_entered(_body):
+	$TinhosoSceneArea/EInteract.visible = true
+
+
+# Esconde a tecla E quando sai para longe do tinhoso
+func _on_TinhosoSceneArea_body_exited(body):
+	$TinhosoSceneArea/EInteract.visible = false
