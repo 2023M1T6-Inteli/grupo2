857010ea72b37ae4d1977c64668b221e63c41148
VictorGM01 <victormarques8801@gmail.com>
2023-03-21 23:01:16 -0300

    feat: incrementa mecânica do minigame de check-list

--- src/CelsoDrive/Scripts/MinigameChecklist.gd
+++ src/CelsoDrive/Scripts/MinigameChecklist.gd
@@ -1,39 +1,39 @@
 extends Node2D
 
-var arrowCrosshair = load("res://Assets/items/crosshair.png")
-const CHECK_ITEM = preload("res://Scenes/CheckItem.tscn")
-var i = 0
+var arrowCrosshair = load("res://Assets/items/crosshair.png") # Imagem da mira
+const CHECK_ITEM = preload("res://Scenes/CheckItem.tscn") # Cena do check item
+var clipboardElement = 0 # Index para adicionar elementos na prancheta
+onready var cont = 0 # Variável para controle do spawn dos itens
+onready var spawnPositions = $SpawnItems/SpawnPositions.get_children()
+var positiveItemsList = ["Dormir bem", "Fazer \nPausas", "Beber água",
+						"Desativar\nnotificações", "Comer bem", "Usar Cinto",
+						"Conversar", "Escutar forró", "Sambar"]
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	Input.set_custom_mouse_cursor(arrowCrosshair)
+	Input.set_custom_mouse_cursor(arrowCrosshair) # Altera cursor do mouse
 
 
 func _on_ItemsTimer_timeout():
-	$Path2D/PathFollow2D.offset = randi()
-	$Path2D/PathFollow2D2.offset = randi()
-	$Path2D/PathFollow2D3.offset = randi()
-	var newItem = CHECK_ITEM.instance()
-	var newItem2 = CHECK_ITEM.instance()
-	var newItem3 = CHECK_ITEM.instance()
-	var newItem4 = CHECK_ITEM.instance()
-	add_child(newItem)
-	add_child(newItem2)
-	add_child(newItem3)
-	add_child(newItem4)
-	newItem.position = $Path2D/PathFollow2D.position
-	newItem.connect("exploded", self, "_on_exploded")
-	newItem2.position = $Path2D/PathFollow2D2.position
-	newItem2.connect("exploded", self, "_on_exploded")
-	newItem3.position = $Path2D/PathFollow2D3.position
-	newItem3.connect("exploded", self, "_on_exploded")
-	newItem4.position = $Path2D/PathFollow2D.position
-	newItem4.connect("exploded", self, "_on_exploded")
-	
+	if cont < 5:
+		if len(positiveItemsList):
+			var indexSpawn = cont
+			var newItem = CHECK_ITEM.instance()
+			newItem.global_position = spawnPositions[indexSpawn].global_position
+			var randomIndex = randi() % positiveItemsList.size()
+			newItem.content = positiveItemsList[randomIndex]
+			add_child(newItem)
+			positiveItemsList.remove(randomIndex)
+			newItem.connect("exploded", self, "_on_exploded")
+			cont += 1
 
-func _on_exploded():
-	$VBoxContainer.get_children()[i].visible = true
-	i += 1
-	
+
+func _on_exploded(currentContent, currentPosition):	
+	if clipboardElement < 5:
+		# Atualiza labels da prancheta quando o check item é destruído
+		var elements = $Clipboard/VBoxContainer.get_children()
+		var element = elements[clipboardElement]
+		element.get_children()[1].text = currentContent
+		element.visible = true
+		clipboardElement += 1
