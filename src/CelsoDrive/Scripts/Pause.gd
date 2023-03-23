9670682a8a0aac5951cfa1742102465ff386894d
VictorGM01 <victormarques8801@gmail.com>
2023-03-23 01:29:23 -0300

    feat: cria tela de pause

--- src/CelsoDrive/Scripts/Pause.gd
+++ src/CelsoDrive/Scripts/Pause.gd
@@ -1,8 +1,29 @@
-extends Control
-
-# CENA EM DESENVOLVIMENTO. NÃO FOI USADA NO ARTEFATO.
+extends CanvasLayer
 
 
-# Abre tela de configurações
-func _on_ConfigButton_pressed():
-	get_tree().change_scene("res://Scenes/SettingsMenu.tscn")
+func _ready():
+	show_pause(false) # Não exibe a tela logo no início
+
+
+func _input(event):
+	# Pausa o jogo quando a tecla esc é pressionada
+	if event.is_action_pressed("ui_cancel"):
+		show_pause(!get_tree().paused)
+		get_tree().paused = not get_tree().paused
+
+
+# Mostra tela de pause
+func show_pause(isVisible):
+	for node in get_children():
+		node.visible = isVisible
+
+
+# Retorna a execução do jogo
+func _on_ContinueButton_pressed():
+	show_pause(false)
+	get_tree().paused = false
+
+
+# Direciona para a casa
+func _on_Home_pressed():
+	return get_tree().change_scene("res://Scenes/OutsideHouse.tscn")
