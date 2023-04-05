153e47fe28ac585bc3f309125196c6c093eeb9d8
VictorGM01 <victormarques8801@gmail.com>
2023-04-05 07:36:08 -0300

    refactor: refatora script dos carros inimigos

--- src/CelsoDrive/Scripts/EnemyCar.gd
+++ src/CelsoDrive/Scripts/EnemyCar.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
 var carSpeed = Global.gameBaseSpeed # Rapidez do movimento
-signal colide
-onready var carTypes = $CarSprite.frames.get_animation_names()
+signal colide # sinal para captar a colisão
+onready var carTypes = $CarSprite.frames.get_animation_names() # seleciona as animações do carro
 
 
 func _ready():
 	randomize()
-	$CarSprite.animation = carTypes[randi() % 3]
+	$CarSprite.animation = carTypes[randi() % 3] # randomiza a animação (cor) do carro
 
 
 func _physics_process(_delta):
@@ -19,8 +19,9 @@ func _on_EnemyCar_body_entered(_body):
 	# Emite "sinal" de pause quando colide com o caminhão
 	Global.pausedGame = true
 	emit_signal("colide")
-	$CarCrash.play()
+	$CarCrash.play() # som de batida
 
 
+# Exclui nó após sair da tela
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
