e089c024a1967fe545ec26f9911939b982e807d8
VictorGM01 <victormarques8801@gmail.com>
2023-04-05 07:32:52 -0300

    refactor: refatora script do check-item

--- src/CelsoDrive/Scripts/CheckItem.gd
+++ src/CelsoDrive/Scripts/CheckItem.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
-signal exploded(currentContent, currentPosition)
-var content: String = "Boa noite\nde sono"
-var initialPosition
-var value
+signal exploded(currentContent, currentPosition) # Sinal para tratar item após ser clicado
+var content: String = "Boa noite\nde sono" # Label do item de tutorial
+var initialPosition # Variável para a posição inicial do item
 
 
 func _ready():
+	# Define a posição do item e o conteúdo da Label
 	initialPosition = global_position
 	$Content.text = content
 
 
+# Emite sinal quando o item recebe um clique.
 func _on_Check_pressed():
 	emit_signal("exploded", String($Content.text), global_position)
 	queue_free()
